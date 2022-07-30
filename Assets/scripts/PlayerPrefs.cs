107982540073e5637029c38816b3cac5344120f7
Keyboard-Genius <56653927+Keyboard-Genius@users.noreply.github.com>
2022-07-29 22:12:13 -0400

    update

---
+++ Assets/scripts/PlayerPrefs.cs
@@ -0,0 +1,8 @@
+public class PlayerPrefs
+{
+    public static int searchDepth { get; set; }
+    public static bool showStats { get; set; }
+
+    public static ulong nodesSearched { get; set; }
+    public static long timeSearched { get; set; }
+}
